269259cf15fe05a6db1b4de28da83771966d9dda
Aude Marêché <aude.mareche@free.fr>
2018-10-06 02:10:43 +0200

    Add functions.R script.

---
+++ functions.R
@@ -0,0 +1,31 @@
+# Sum values in a column of a data frame.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d, provided as a string
+#
+# RETURN VALUE:
+# if the specified column exists and contains numbers, returns the sum of
+# all values in the column; otherwise, returns NULL
+sum_column <- function(d, var) {
+  # Set a default value to return
+  result <- NULL
+  x <- d[[var]] # Remember, as we showed in class: there are two ways
+  # to access a column, and this is one; you should try
+  # and figure out why the other way won't work here,
+  # but that's not part of the homework
+  if (!is.null(x)) { # This tests to see whether the column exists in
+    # d; again, you should try and find a way to test
+    # this out for yourself to verify that it's true,
+    # but that's not part of the homework
+    if (is.numeric(x)) {
+      count = 0
+      for (i in x) {
+        count <- count + i
+      }
+      result <- count
+    }
+    # You will need to do a bit of research in order to figure out how
+    # to test whether a vector contains numbers.
+  }
+}
