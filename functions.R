662a6b6730c4e473bb5f00dafdfa1ab734261edf
Aude Marêché <aude.mareche@free.fr>
2018-10-06 02:25:49 +0200

    Add answer to sub-exercise 3-b.

--- functions.R
+++ functions.R
@@ -29,3 +29,29 @@ sum_column <- function(d, var) {
     # to test whether a vector contains numbers.
   }
 }
+
+# Sum values in a vector.
+#
+# ARGUMENTS:
+# x: a vector
+#
+# RETURN VALUE:
+# if the vector contains numbers, returns the sum of
+# all values; otherwise, returns NULL
+#
+my_sum <- function(x) {
+  # Assign a default value to the result
+  result <- NULL
+  # Check if x contains numeric values
+  if (is.numeric(x)) {
+    # Initialise our count to 0
+    count = 0
+    # Go through every value in x
+    for (i in x) {
+      # Add every value in x to the total count, one at a time
+      count <- count + i
+    }
+    # We calculated the sum, so store it in the result to be returned
+    result <- count
+  }
+}
